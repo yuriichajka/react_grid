075ff099dd33120effc39371412d1afef406ea4e
yuriichajka <yuriichajka@gmail.com>
2021-09-03 16:19:05 +0300

    Custom Cell Value Setters

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,92 @@
 import React, {useState} from 'react';
 import Paper from '@material-ui/core/Paper';
+import { EditingState } from '@devexpress/dx-react-grid';
 import {
     Grid,
     Table,
     TableHeaderRow,
+    TableEditRow,
+    TableEditColumn,
 } from '@devexpress/dx-react-grid-material-ui';
-import {
-    SortingState,
-    IntegratedSorting,
-} from '@devexpress/dx-react-grid';
-import { generateRows } from './demo-data/generator';
+import { generateRows, defaultNestedColumnValues } from './demo-data/generator';
 
-
+// @ts-ignore
+const getRowId = row => row.id;
 
 function App() {
     const [columns] = useState([
-        { name: 'name', title: 'Name' },
-        { name: 'gender', title: 'Gender' },
+        {
+            name: 'firstName',
+            title: 'First Name',
+            // @ts-ignore
+            getCellValue: row => (row.user ? row.user.firstName : undefined),
+        },
+        {
+            name: 'lastName',
+            title: 'Last Name',
+            // @ts-ignore
+            getCellValue: row => (row.user ? row.user.lastName : undefined),
+        },
+        {
+            name: 'car',
+            title: 'Car',
+            // @ts-ignore
+            getCellValue: row => (row.car ? row.car.model : undefined),
+        },
+        { name: 'position', title: 'Position' },
         { name: 'city', title: 'City' },
-        { name: 'car', title: 'Car' },
     ]);
-    const [rows] = useState(generateRows({ length: 8 }));
-    const [sorting, setSorting] = useState([{ columnName: 'car', direction: 'asc' }]);
+    const [rows, setRows] = useState(generateRows({
+        // @ts-ignore
+        columnValues: { id: ({ index }) => index, ...defaultNestedColumnValues },
+        length: 8,
+    }));
+    const [editingColumnExtensions] = useState([
+        {
+            columnName: 'firstName',
+            // @ts-ignore
+            createRowChange: (row, value) => ({ user: { ...row.user, firstName: value } }),
+        },
+        {
+            columnName: 'lastName',
+            // @ts-ignore
+            createRowChange: (row, value) => ({ user: { ...row.user, lastName: value } }),
+        },
+        {
+            columnName: 'car',
+            // @ts-ignore
+            createRowChange: (row, value) => ({ car: { model: value } }),
+        },
+    ]);
+
+    // @ts-ignore
+    const commitChanges = ({ added, changed, deleted }) => {
+        let changedRows;
+        if (added) {
+            // @ts-ignore
+            const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
+            changedRows = [
+                ...rows,
+                // @ts-ignore
+                ...added.map((row, index) => ({
+                    id: startingAddedId + index,
+                    ...row,
+                })),
+            ];
+        }
+        if (changed) {
+            // @ts-ignore
+            changedRows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+        }
+        if (deleted) {
+            const deletedSet = new Set(deleted);
+            // @ts-ignore
+            changedRows = rows.filter(row => !deletedSet.has(row.id));
+        }
+        // @ts-ignore
+        setRows(changedRows);
+    };
+
 
     return (
       <div>
@@ -30,14 +95,15 @@ function App() {
                   rows={rows}
                   columns={columns}
               >
-                  <SortingState
+                  <EditingState
+                      columnExtensions={editingColumnExtensions}
                       // @ts-ignore
-                      sorting={sorting}
-                      onSortingChange={setSorting}
+                      onCommitChanges={commitChanges}
                   />
-                  <IntegratedSorting />
                   <Table />
-                  <TableHeaderRow showSortingControls />
+                  <TableHeaderRow />
+                  <TableEditRow />
+                  <TableEditColumn showAddCommand showEditCommand showDeleteCommand />
               </Grid>
           </Paper>
       </div>
